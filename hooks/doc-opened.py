8141b596797bb7b111478a02b748a93306827173
Pete Heibel <peterjamesheibel@gmail.com>
2023-04-13 10:44:39 -0700

    updated central model warning

--- hooks/doc-opened.py
+++ hooks/doc-opened.py
@@ -4,8 +4,8 @@ from pyrevit import EXEC_PARAMS
 from Autodesk.Revit.DB import ModelPathUtils
 
 title = "Warning!"
-message = "You've opened the central model. Run!"
-body = "Click OK. Do not save. Close the file immediately."
+message = "You've opened the central model. OMG!"
+body = "All will be fine. Simply click OK, do not save, and close out of the file. Try creating a local from this file instead."
 
 if EXEC_PARAMS.event_args.Document.IsWorkshared:
 
@@ -18,7 +18,8 @@ if EXEC_PARAMS.event_args.Document.IsWorkshared:
     if  currentDocPath == modifiedCentralPath:
         forms.alert(message,title,body)
     else:
-        forms.alert("Nothing is wrong at all. Carry on.","Central Model Check", "Central File Path: \n" + modifiedCentralPath,"Current File Path: \n" + currentDocPath)
+        pass
+        #forms.alert("Nothing is wrong at all. Carry on.","Central Model Check", "Central File Path: \n" + modifiedCentralPath,"Current File Path: \n" + currentDocPath)
 
 else:
     pass
