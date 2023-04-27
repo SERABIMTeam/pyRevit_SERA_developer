d98e92dc573c1aafbee68afdc33e5bd216e26007
Pete Heibel <peterjamesheibel@gmail.com>
2023-04-26 19:54:45 -0700

    test

--- SERA.tab/Developer.panel/Add Worksets.pushbutton/script.py
+++ SERA.tab/Developer.panel/Add Worksets.pushbutton/script.py
@@ -1,4 +1,4 @@
-"""Create worksets from a list and set default visibility"""
+"""Create SERA Worksets"""
 import clr
 clr.AddReference("RevitServices")
 import RevitServices
@@ -7,48 +7,29 @@ from RevitServices.Transactions import TransactionManager
 
 clr.AddReference("RevitAPI")
 from Autodesk.Revit import DB
+clr.AddReference("RevitAPIUI")
+from Autodesk.Revit.UI import TaskDialog
 
-doc =  DocumentManager.Instance.CurrentDBDocument
+doc = DocumentManager.Instance.CurrentDBDocument
 
 def create_worksets(d, names):
     for name in names:
         ws = DB.Workset.Create(d, name)
 
-workset_names = ["DATUM ELEMENTS", "MAIN MODEL", "UNITS", "X_A_SITE_RVT", "X_M_MECHANICAL_RVT", "X_S_STRUCTURAL_RVT"]
+workset_names = ["UNITS", "X_A_SITE_RVT", "X_M_MECHANICAL_RVT", "X_S_STRUCTURAL_RVT"]
 
 # enable worksharing
-if not doc.IsWorkShared:
-      WorksharingUtils.EnableWorksharing(doc)
+if not doc.IsWorkshared:
+    WorksharingUtils.EnableWorksharing("DATUM ELEMENTS", "MAIN MODEL")
 
 if doc.IsWorkshared:
     t = DB.Transaction(doc, "Create Worksets")
     t.Start()
-    
     # make your model changes here
     create_worksets(doc, workset_names)
     t.Commit()
     
-    
-    
-    
-# #chatGPT
-
-# # check if the document is already workshared
-# if not doc.IsWorkshared:
-#     # make the document workshared
-#     WorksharingUtils.EnableWorksharing(doc)
-
-# # get the worksharing options
-# options = WorksharingSaveAsOptions()
-# options.SaveAsCentral = True
-
-# # save the file as a central file
-# doc.SaveAs(doc.PathName, options)
-
-# # add a list of worksets
-# workset_names = ["Workset1", "Workset2", "Workset3"]
-# for name in workset_names:
-#     Workset.Create(doc, name)
-
-# # save the changes to the file
-# doc.Save()
+message = TaskDialog()
+message.Title = "Worksets Added"
+message.MainInstruction = "SERA standard worksets have been added to this file. Next Steps:\n\n1. Save file to project folder\n\n2. Select Collaborate > Relinquish All Mine\n\n3. Select File > Close"
+message.Show()
